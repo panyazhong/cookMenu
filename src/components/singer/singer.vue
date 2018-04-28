1865bec09b91ed24fe1a1762fd5786621e6e50fa
panyazhong <pyz1226149198@gmail.com>
2018-04-28 17:45:57 +0800

    add getSingerList and sort singerList

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -4,8 +4,12 @@
 </template>
 
 <script>
-  import {getSingerList} from "api/singer";
-  import {ERR_OK} from "api/config";
+  import {getSingerList} from "api/singer"
+  import {ERR_OK} from "api/config"
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_NAME_LEN = 10
 
   export default {
     data() {
@@ -19,11 +23,58 @@
     methods: {
       _getSingerList() {
         getSingerList().then((res) => {
-          console.log(res)
           if (res.code === ERR_OK) {
-            this.singers = res.data.list
+            this.singers = this._normalizeSinger(res.data.list)
+            console.log(this.singers)
           }
         })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_NAME_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+
+          let key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        //将map进行排序
+        let hot = []
+        let ret = []
+
+        for(let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title == HOT_NAME) {
+            hot.push(val)
+          }
+        }
+
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+
+        return hot.concat(ret)
       }
     }
   }
